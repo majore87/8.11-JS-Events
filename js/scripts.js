782e3e0275c4328516065fa71695645fcfd7883f
Piotr <piotr.krzysztof.wojcik@gmail.com>
2017-02-27 23:26:36 +0100

    JS exercise completed with correction

--- js/scripts.js
+++ js/scripts.js
@@ -4,18 +4,17 @@ function checkOnclickEvent(param) {
 
 var buttonElem = document.getElementById('addElem');
 
+var itemLength = 0;
+
 buttonElem.addEventListener('click', function(e) {
 
 	var unorderedList = document.getElementById('list');
 	var newElem = document.createElement('li');
-	newElem.innerHTML += '<li>item</li>';
-	var childrenUl = unorderedList.length;
+
+	itemLength ++;
+	newElem.innerHTML += '<li>item ' + itemLength + ' </li>';
 
 	unorderedList.appendChild(newElem);
-
-	// var ulByTagName = document.getElementsByTagName('li');
-	// var numbersOfUl = ulByTagName.length;
-	// console.log(numbersOfUl);
 
 	checkOnclickEvent("Item added!");
 });
